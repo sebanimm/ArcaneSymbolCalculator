00705ecf5f0c5e29196f0eb01d116566f5c08af2
sebanimm <gks32114221@gmail.com>
2022-06-19 14:40:47 +0900

    2022.06.18 | 오류 수정

--- ArcSymbolCal.py
+++ ArcSymbolCal.py
@@ -10,60 +10,60 @@ print("""
 6. 에스페라
 """.strip())
 
-SymType = int(input("아케인 심볼의 종류: "))
-SymLv = int(input("아케인 심볼의 레벨: "))
+symType = int(input("아케인 심볼의 종류: "))
+symLv = int(input("아케인 심볼의 레벨: "))
 
-UserSym = int(input("아케인 심볼의 갯수: "))
-UserMeso_10K = int(input("메소 (억 단위): "))
-UserMeso_100M = int(input("메소 (만 단위): "))
+userSym = int(input("아케인 심볼의 갯수: "))
+userMeso_10K = int(input("메소 (억 단위): "))
+userMeso_100M = int(input("메소 (만 단위): "))
 
 maxSym = Arcane(20)
 maxLv = maxSym.AccNeedLv()
 
-UserMeso_10K *= 10000
-UserMesoTotal = UserMeso_10K + UserMeso_100M
+userMeso_10K *= 10000
+userMesoTotal = userMeso_10K + userMeso_100M
 
-Symbol = Arcane(SymLv)
+symbol = Arcane(symLv)
 
-print(f"만렙까지 더 필요한 아케인 심볼은 {((maxLv) - Arcane(SymLv).AccNeedLv()) - UserSym} 개 입니다")
-
-# symbolMeso->심볼 업그레이드 가격 (억단위 + 만단위) (1/2/3/4,5,6)
-symbolMeso_1stSym = (Symbol.NeedMeso_100M_1stSym()*10000) + Symbol.NeedMeso_10K_1stSym()
-symbolMeso_2ndSym = (Symbol.NeedMeso_100M_2ndSym()*10000) + Symbol.NeedMeso_10K_2ndSym()
-symbolMeso_3rdSym = (Symbol.NeedMeso_100M_3rdSym()*10000) + Symbol.NeedMeso_10K_3rdSym()
-symbolMeso_otherSym = (Symbol.NeedMeso_100M_otherSym()*10000) + Symbol.NeedMeso_10K_otherSym()
+print(f"만렙까지 더 필요한 아케인 심볼은 {((maxLv) - Arcane(symLv).AccNeedLv()) - userSym}개 입니다")
 
 # resultMeso_100M -> 억단위메소, resultMeso_10K -> 만단위 메소 
-resultMeso_100M_1stSym = (Symbol.NeedMeso_100M_1stSym()*10000) - UserMeso_10K # 필요한 억단위 메소 (1)
-resultMeso_100M_2ndSym = (Symbol.NeedMeso_100M_2ndSym()*10000) - UserMeso_10K # 필요한 억단위 메소 (2)
-resultMeso_100M_3rdSym = (Symbol.NeedMeso_100M_3rdSym()*10000) - UserMeso_10K # 필요한 억단위 메소 (3)
-resultMeso_100M_otherSym = (Symbol.NeedMeso_100M_otherSym()*10000) - UserMeso_10K # 필요한 억단위 메소 (4,5,6)
-resultMeso_10K_1stSym = Symbol.NeedMeso_10K_1stSym() - UserMeso_100M # 필요한 만단위 메소 (1)
-resultMeso_10K_2ndSym = Symbol.NeedMeso_10K_2ndSym() - UserMeso_100M # 필요한 만단위 메소 (2)
-resultMeso_10K_3rdSym = Symbol.NeedMeso_10K_3rdSym() - UserMeso_100M # 필요한 만단위 메소 (3)
-resultMeso_10K_otherSym = Symbol.NeedMeso_10K_otherSym() - UserMeso_100M # 필요한 만단위 메소 (4,5,6)
+resultMeso_10K_1stSym = symbol.needMeso_10K_1stSym() - userMeso_100M # 필요한 만단위 메소 (1)
+resultMeso_10K_2ndSym = symbol.needMeso_10K_2ndSym() - userMeso_100M # 필요한 만단위 메소 (2)
+resultMeso_10K_3rdSym = symbol.needMeso_10K_3rdSym() - userMeso_100M # 필요한 만단위 메소 (3)
+resultMeso_10K_otherSym = symbol.needMeso_10K_otherSym() - userMeso_100M # 필요한 만단위 메소 (4,5,6)
+resultMeso_100M_1stSym = (symbol.needMeso_100M_1stSym()*10000) - userMeso_10K # 필요한 억단위 메소 (1)
+resultMeso_100M_2ndSym = (symbol.needMeso_100M_2ndSym()*10000) - userMeso_10K # 필요한 억단위 메소 (2)
+resultMeso_100M_3rdSym = (symbol.needMeso_100M_3rdSym()*10000) - userMeso_10K # 필요한 억단위 메소 (3)
+resultMeso_100M_otherSym = (symbol.needMeso_100M_otherSym()*10000) - userMeso_10K # 필요한 억단위 메소 (4,5,6)
+
+# symbolMeso->심볼 업그레이드 가격 (억단위 + 만단위) (1/2/3/4,5,6)
+symbolMeso_1stSym = (symbol.needMeso_100M_1stSym()*10000) + symbol.needMeso_10K_1stSym()
+symbolMeso_2ndSym = (symbol.needMeso_100M_2ndSym()*10000) + symbol.needMeso_10K_2ndSym()
+symbolMeso_3rdSym = (symbol.needMeso_100M_3rdSym()*10000) + symbol.needMeso_10K_3rdSym()
+symbolMeso_otherSym = (symbol.needMeso_100M_otherSym()*10000) + symbol.needMeso_10K_otherSym()
    
-if SymType == 1 :
+if symType == 1 :
     if resultMeso_10K_1stSym < 0 :
         resultMeso_10K_1stSym += 10000
         resultMeso_100M_1stSym -= 1
-    if UserMesoTotal < symbolMeso_1stSym :
+    if userMesoTotal < symbolMeso_1stSym :
         print(f"만렙까지 더 필요한 메소는 {math.trunc(resultMeso_100M_1stSym / 10000)}억 {resultMeso_10K_1stSym}만 메소입니다")
     else :
         print("만렙까지 필요한 메소는 충분합니다")
-elif SymType == 2 :
+elif symType == 2 :
     if resultMeso_10K_2ndSym < 0 :
         resultMeso_10K_2ndSym += 10000
         resultMeso_100M_2ndSym -= 1
-    if UserMesoTotal < symbolMeso_2ndSym :
+    if userMesoTotal < symbolMeso_2ndSym :
         print(f"만렙까지 더 필요한 메소는 {math.trunc(resultMeso_100M_2ndSym / 10000)}억 {resultMeso_10K_2ndSym}만 메소입니다")
     else :
         print("만렙까지 필요한 메소는 충분합니다")
-elif SymType == 3 :
+elif symType == 3 :
     if resultMeso_10K_3rdSym < 0 :
         resultMeso_10K_3rdSym += 10000
         resultMeso_100M_3rdSym -= 1
-    if UserMesoTotal < symbolMeso_3rdSym :
+    if userMesoTotal < symbolMeso_3rdSym :
         print(f"만렙까지 더 필요한 메소는 {math.trunc(resultMeso_100M_3rdSym / 10000)}억 {resultMeso_10K_3rdSym}만 메소입니다")
     else :
         print("만렙까지 필요한 메소는 충분합니다")
@@ -71,7 +71,7 @@ else:
     if resultMeso_10K_otherSym < 0 :
         resultMeso_10K_otherSym += 10000
         resultMeso_100M_otherSym -= 1
-    if UserMesoTotal < symbolMeso_otherSym :
+    if userMesoTotal < symbolMeso_otherSym :
         print(f"만렙까지 더 필요한 메소는 {math.trunc(resultMeso_100M_otherSym / 10000)}억 {resultMeso_10K_otherSym}만 메소입니다")
     else :
         print("만렙까지 필요한 메소는 충분합니다")
--- Arcane.py
+++ Arcane.py
@@ -13,105 +13,107 @@ class Arcane:
             AccLv += (lv**2) + 11
         return AccLv
 
-        # NeedMeso_100M -> 더 필요한 억단위 메소
-        # MaxLvMeso -> 만렙까지 드는 돈
-        # DefaultMeso -> 기본적으로 드는 메소
+        # needMeso_100M -> 더 필요한 억단위 메소
+        # maxLvMeso -> 만렙까지 드는 돈
+        # defaultMeso -> 기본적으로 드는 메소
 
-    def NeedMeso_100M_1stSym(self):
-        DefaultMeso = 3110000 
-        NeedMeso_100M = 0
-        MaxLvMeso = 811490000 
+    def needMeso_100M_1stSym(self):
+        defaultMeso = 3110000 
+        needMeso_100M = 0
+        maxLvMeso = 811490000 
         for lv in range(1, self.level):
-            DefaultMeso += 3960000
-            NeedMeso_100M += DefaultMeso
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_100M = math.trunc(NeedMeso_100M / 100000000)
-        return NeedMeso_100M
+            defaultMeso += 3960000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_100M = math.trunc(needMeso_100M / 100000000)
+        return needMeso_100M
 
-    def NeedMeso_100M_2ndSym(self):
-        DefaultMeso = 6220000
-        NeedMeso_100M = 0
-        MaxLvMeso = 995980000
+    def needMeso_100M_2ndSym(self):
+        defaultMeso = 6220000
+        needMeso_100M = 0
+        maxLvMeso = 995980000
         for lv in range(1, self.level):
-            DefaultMeso += 4620000
-            NeedMeso_100M += DefaultMeso
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_100M = math.trunc(NeedMeso_100M / 100000000)
-        return NeedMeso_100M
+            defaultMeso += 4620000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_100M = math.trunc(needMeso_100M / 100000000)
+        return needMeso_100M
 
-    def NeedMeso_100M_3rdSym(self):
-        DefaultMeso = 9330000
-        NeedMeso_100M = 0
-        MaxLvMeso = 1180470000
+    def needMeso_100M_3rdSym(self):
+        defaultMeso = 9330000
+        needMeso_100M = 0
+        maxLvMeso = 1180470000
         for lv in range(1, self.level):
-            DefaultMeso += 5280000
-            NeedMeso_100M += DefaultMeso
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_100M = math.trunc(NeedMeso_100M / 100000000)
-        return NeedMeso_100M
+            defaultMeso += 5280000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_100M = math.trunc(needMeso_100M / 100000000)
+        return needMeso_100M
 
-    def NeedMeso_100M_otherSym(self):
-        DefaultMeso = 11196000
-        NeedMeso_100M = 0
-        MaxLvMeso = 1341324000
+    def needMeso_100M_otherSym(self):
+        defaultMeso = 11196000
+        needMeso_100M = 0
+        maxLvMeso = 1341324000
         for lv in range(1, self.level):
-            DefaultMeso += 5940000 
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_100M = math.trunc(NeedMeso_100M / 100000000)
-        return NeedMeso_100M
+            defaultMeso += 5940000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_100M = math.trunc(needMeso_100M / 100000000)
+        return needMeso_100M
 
-        # NeedMeso_10K -> 더 필요한 만단위 메소
-        # MaxLvMeso -> 만렙까지 드는 돈
-        # DefaultMeso -> 기본적으로 드는 메소
+        # needMeso_10K -> 더 필요한 만단위 메소
+        # maxLvMeso -> 만렙까지 드는 돈
+        # defaultMeso -> 기본적으로 드는 메소
         
-    def NeedMeso_10K_1stSym(self):
-        DefaultMeso = 3110000 #
-        NeedMeso_100M = 0
-        NeedMeso_10K = 0
-        MaxLvMeso = 811490000 
+    def needMeso_10K_1stSym(self):
+        defaultMeso = 3110000 #
+        needMeso_100M = 0
+        needMeso_10K = 0
+        maxLvMeso = 811490000 
         for lv in range(1, self.level):
-            DefaultMeso += 3960000
-            NeedMeso_100M += DefaultMeso
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_10K += math.trunc(NeedMeso_100M / 10000)
-        NeedMeso_10K %= 10000
-        return NeedMeso_10K
+            defaultMeso += 3960000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_10K += math.trunc(needMeso_100M / 10000)
+        needMeso_10K %= 10000
+        return needMeso_10K
 
-    def NeedMeso_10K_2ndSym(self):
-        DefaultMeso = 6220000
-        NeedMeso_100M = 0
-        NeedMeso_10K = 0
-        MaxLvMeso = 995980000
+    def needMeso_10K_2ndSym(self):
+        defaultMeso = 6220000
+        needMeso_100M = 0
+        needMeso_10K = 0
+        maxLvMeso = 995980000
         for lv in range(1, self.level):
-            DefaultMeso += 4620000
-            NeedMeso_100M += DefaultMeso
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_10K += math.trunc(NeedMeso_100M / 10000)
-        NeedMeso_10K %= 10000
-        return NeedMeso_10K
+            defaultMeso += 4620000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_10K += math.trunc(needMeso_100M / 10000)
+        needMeso_10K %= 10000
+        return needMeso_10K
         
 
-    def NeedMeso_10K_3rdSym(self):
-        DefaultMeso = 9330000
-        NeedMeso_100M = 0
-        NeedMeso_10K = 0
-        MaxLvMeso = 1180470000
+    def needMeso_10K_3rdSym(self):
+        defaultMeso = 9330000
+        needMeso_100M = 0
+        needMeso_10K = 0
+        maxLvMeso = 1180470000
         for lv in range(1, self.level):
-            DefaultMeso += 5280000
-            NeedMeso_100M += DefaultMeso
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_10K += math.trunc(NeedMeso_100M / 10000)
-        NeedMeso_10K %= 10000
-        return NeedMeso_10K
+            defaultMeso += 5280000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_10K += math.trunc(needMeso_100M / 10000)
+        needMeso_10K %= 10000
+        return needMeso_10K
 
-    def NeedMeso_10K_otherSym(self):
-        DefaultMeso = 11196000
-        NeedMeso_100M = 0
-        NeedMeso_10K = 0
-        MaxLvMeso = 1341324000
+    def needMeso_10K_otherSym(self):
+        defaultMeso = 11196000
+        needMeso_100M = 0
+        needMeso_10K = 0
+        maxLvMeso = 1341324000
         for lv in range(1, self.level):
-            DefaultMeso += 5940000 
-        NeedMeso_100M = MaxLvMeso - NeedMeso_100M
-        NeedMeso_10K += math.trunc(NeedMeso_100M / 10000)
-        NeedMeso_10K %= 10000
-        return NeedMeso_10K
+            defaultMeso += 5940000
+            needMeso_100M += defaultMeso
+        needMeso_100M = maxLvMeso - needMeso_100M
+        needMeso_10K += math.trunc(needMeso_100M / 10000)
+        needMeso_10K %= 10000
+        return needMeso_10K
